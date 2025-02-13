d732cface13a94e165bd7d39ba4eafbcdb9c2882
Adrian <adrian.daeschlein@online.de>
2025-02-13 12:32:57 +0100

    FIX geophone settings

--- arduino/geophone_basic.ino
+++ arduino/geophone_basic.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
-// need to install "Adafruit ADS1X15 by Adafruit" library
+
 Adafruit_ADS1115 ads; // Create an ADS1115 instance
 
 void setup()
@@ -8,29 +8,21 @@ void setup()
     Serial.begin(115200);
     while (!Serial)
         ; // Wait for Serial Monitor to open
-    Serial.println("Initializing ADS1115...");
+    // Serial.println("Initializing ADS1115...");
 
     if (!ads.begin())
     {
-        Serial.println("Failed to initialize ADS1115!");
+        // Serial.println("Failed to initialize ADS1115!");
         while (1)
             ;
     }
 
-    /*
-    Gain sets the sensitivity of the ADC. The higher the gain, the more sensitive the ADC is to small changes in voltage.
-    Possible values:
-    GAIN_TWOTHIRDS (±6.144V)
-    GAIN_ONE (±4.096V)
-    GAIN_TWO (±2.048V)
-    GAIN_FOUR (±1.024V)
-    GAIN_EIGHT (±0.512V)
-    GAIN_SIXTEEN (±0.256V)
-     */
-    // ads.setGain(GAIN_ONE);
+    // Set Gain: 0 = ±6.144V, 1 = ±4.096V, 2 = ±2.048V, 4 = ±1.024V, 8 = ±0.512V, 16 = ±0.256V
+    // ads.setGain(GAIN_ONE);  // ±4.096V range (adjust as needed)
     ads.setGain(GAIN_EIGHT);
+    ads.setDataRate(RATE_ADS1115_475SPS);
 
-    Serial.println("ADS1115 initialized.");
+    // Serial.println("ADS1115 initialized.");
 }
 
 void loop()
